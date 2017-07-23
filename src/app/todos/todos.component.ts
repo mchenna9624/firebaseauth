13193b4a71d774d2e323e4cb96b5f15a1d1457db
Madhu Chenna <mchennai@amfam.com>
2017-07-23 16:48:08 -0500

    CRUD Operations included

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -8,10 +8,10 @@ import {TodoService} from './todo.service';
   providers: [TodoService]
 })
 export class TodosComponent implements OnInit {
-  private todos;
-  private activeTasks;
-  private newTodo;
-  private inactiveTasks;
+  public todos;
+  public activeTasks;
+  public newTodo;
+  public inactiveTasks;
 
 
 
